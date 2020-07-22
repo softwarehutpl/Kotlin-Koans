712792ca297911dff3b5b47b4ca97bfb4bce7b78
Daniel Łojewski <daniel.lojewski@softwarehut.com>
2020-07-22 12:03:34 +0200

    feat: improvement to handler test

--- app/src/test/java/com/softwarehut/mobile/kotlin_koans/i_coroutines/_2_error_handling/N02cHandlerKtTest.kt
+++ app/src/test/java/com/softwarehut/mobile/kotlin_koans/i_coroutines/_2_error_handling/N02cHandlerKtTest.kt
@@ -1,8 +1,6 @@
 package com.softwarehut.mobile.kotlin_koans.i_coroutines._2_error_handling
 
-import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.delay
-import kotlinx.coroutines.launch
 import kotlinx.coroutines.runBlocking
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
@@ -10,10 +8,10 @@ import org.junit.jupiter.api.Test
 class N02cHandlerKtTest {
     @Test
     fun testOk() {
-        GlobalScope.launch {
+        runBlocking {
             task2c()
+            delay(1_000)
+            assertEquals("OK", valueToTest)
         }
-        Thread.sleep(1_000)
-        assertEquals("OK", valueToTest)
     }
 }
