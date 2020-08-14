a23eb00affe34deca1fc1918a7cbcff17b35f163
Daniel Łojewski <daniel.lojewski@softwarehut.com>
2020-08-14 13:00:01 +0200

    feat: scope koan

--- app/src/main/java/com/softwarehut/mobile/kotlin_koans/i_coroutines/_0_hello_world/N00dScope.kt
+++ app/src/main/java/com/softwarehut/mobile/kotlin_koans/i_coroutines/_0_hello_world/N00dScope.kt
@@ -1,2 +1,11 @@
 package com.softwarehut.mobile.kotlin_koans.i_coroutines._0_hello_world
 
+import kotlinx.coroutines.*
+
+private val todo = """
+    
+"""
+
+fun task0d(): CoroutineScope {
+    return CoroutineScope(CoroutineName("LearningScopes"))
+}
--- app/src/test/java/com/softwarehut/mobile/kotlin_koans/i_coroutines/_0_hello_world/N00dScopeTest.kt
+++ app/src/test/java/com/softwarehut/mobile/kotlin_koans/i_coroutines/_0_hello_world/N00dScopeTest.kt
@@ -1,2 +1,23 @@
 package com.softwarehut.mobile.kotlin_koans.i_coroutines._0_hello_world
 
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.runBlocking
+import org.junit.jupiter.api.Assertions
+import org.junit.jupiter.api.Test
+
+class N00dScopeTest {
+    @Test
+    fun testOk() {
+        runBlocking {
+            val coroutineScope = task0d()
+            var scopeName = ""
+            coroutineScope.launch {
+                scopeName = Thread.currentThread().toString()
+            }.join()
+            Assertions.assertEquals(
+                true,
+                scopeName.contains("LearningScopes".toRegex())
+            )
+        }
+    }
+}
