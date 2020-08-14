0d9134c45d10ae8964d5b22a2578f19df2aa8fbf
Daniel Łojewski <daniel.lojewski@softwarehut.com>
2020-08-14 13:52:24 +0200

    feat: Flow Koan test

--- app/src/main/java/com/softwarehut/mobile/kotlin_koans/i_coroutines/_5_flow/N05Flow.kt
+++ app/src/main/java/com/softwarehut/mobile/kotlin_koans/i_coroutines/_5_flow/N05Flow.kt
@@ -1,2 +1,8 @@
 package com.softwarehut.mobile.kotlin_koans.i_coroutines._5_flow
 
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.*
+
+fun task5(): Flow<Int> {
+    return (0 .. 100).asFlow().onEach { delay(100) }
+}
