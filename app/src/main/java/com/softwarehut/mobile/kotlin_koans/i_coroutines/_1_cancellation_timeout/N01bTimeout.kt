655e96737eb42d612141bfad5df34fca67221694
Daniel Łojewski <daniel.lojewski@softwarehut.com>
2020-08-14 13:34:36 +0200

    feat: timeout koan is here

--- app/src/main/java/com/softwarehut/mobile/kotlin_koans/i_coroutines/_1_cancellation_timeout/N01bTimeout.kt
+++ app/src/main/java/com/softwarehut/mobile/kotlin_koans/i_coroutines/_1_cancellation_timeout/N01bTimeout.kt
@@ -1,2 +1,23 @@
 package com.softwarehut.mobile.kotlin_koans.i_coroutines._1_cancellation_timeout
 
+import kotlinx.coroutines.*
+
+fun task1b(): Job {
+    return GlobalScope.launch {
+        withTimeout(1_000) {
+            launch {
+                anotherLongRunningWork()
+            }
+        }
+    }
+}
+
+suspend fun anotherLongRunningWork() {
+    withContext(Dispatchers.IO) {
+        var counter = 0L
+        for (i in 0..100_000_000_000) {
+            if (isActive.not()) { return@withContext }
+            counter++
+        }
+    }
+}
