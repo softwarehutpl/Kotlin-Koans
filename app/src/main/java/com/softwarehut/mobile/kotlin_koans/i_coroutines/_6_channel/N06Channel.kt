3a0f9ae0887a676d58d6ebb1729a4707d7cd1b42
Daniel Łojewski <daniel.lojewski@softwarehut.com>
2020-08-14 16:07:45 +0200

    feat: Ping Pong as Channel Koan ripped of from the official docs

--- app/src/main/java/com/softwarehut/mobile/kotlin_koans/i_coroutines/_6_channel/N06Channel.kt
+++ app/src/main/java/com/softwarehut/mobile/kotlin_koans/i_coroutines/_6_channel/N06Channel.kt
@@ -1,2 +1,15 @@
 package com.softwarehut.mobile.kotlin_koans.i_coroutines._6_channel
 
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.channels.Channel
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+
+fun task6(table: Channel<String>) {
+    GlobalScope.launch {
+        for (ball in table) {
+            delay(256)
+            table.send("Pong")
+        }
+    }
+}
--- app/src/test/java/com/softwarehut/mobile/kotlin_koans/i_coroutines/_6_channel/N06ChannelTest.kt
+++ app/src/test/java/com/softwarehut/mobile/kotlin_koans/i_coroutines/_6_channel/N06ChannelTest.kt
@@ -1,2 +1,27 @@
 package com.softwarehut.mobile.kotlin_koans.i_coroutines._6_channel
 
+import kotlinx.coroutines.*
+import kotlinx.coroutines.channels.Channel
+import org.junit.jupiter.api.Assertions
+import org.junit.jupiter.api.Test
+
+class N06ChannelTest {
+    @Test
+    fun testOk() {
+        GlobalScope.launch {
+            val table = Channel<String>()
+            launch {
+                for (ball in table) {
+                    Assertions.assertEquals("Pong", ball)
+                    println(ball)
+                    delay(256)
+                    println("Ping")
+                    table.send("Ping")
+                }
+            }
+            task6(table)
+            table.send("Pong")
+        }
+        Thread.sleep(4_096)
+    }
+}
